feafa7cfaf2d36355c7133e23db21999abe07cbc
loifav <favrelc22@gmail.com>
2024-12-04 15:51:29 +0100

    changed links into treeMapData.ts

--- src/modules/dashboard/components/freightforwarder/data/treeMapData.ts
+++ src/modules/dashboard/components/freightforwarder/data/treeMapData.ts
@@ -1,9 +1,8 @@
 
 export const treemapData = [
-    { name: "Invoices to validate", count: 10, link: "/transactions?tab=toValidate", fill: "#223c60" },
-    { name: "Invoices validated", count: 16, link: "/transactions?tab=validated", fill: "#223c60" },
-    { name: "Invoices overdue", count: 10, link: "/transactions?tab=dueAndOverdue", fill: "#b30000" },
-    { name: "Invoices paid", count: 12, link: "/transactions?tab=paid", fill: "#006400" },
-    { name: "Invoices due", count: 15, link: "/transactions?tab=dueAndOverdue", fill: "#cc7a00" },
+    { name: "Invoices to validate", count: 10, link: "/invoices?tab=toValidate", fill: "#223c60" },
+    { name: "Invoices validated", count: 16, link: "/invoices?tab=validated", fill: "#223c60" },
+    { name: "Invoices overdue", count: 10, link: "/invoices?tab=dueAndOverdue", fill: "#b30000" },
+    { name: "Invoices paid", count: 12, link: "/invoices?tab=paid", fill: "#006400" },
+    { name: "Invoices due", count: 15, link: "/invoices?tab=dueAndOverdue", fill: "#cc7a00" },
   ];
-  
