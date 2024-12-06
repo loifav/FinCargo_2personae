28ccf586e6732197e00f898a6feb45f6ee30d580
Ant1Be <antoine.bellon@hes-so.ch>
2024-12-06 02:32:50 +0100

    clean @mokes

--- src/modules/invoices/components/freightforwarder/status/transactionPast/PastTransactionFileList.tsx
+++ src/modules/invoices/components/freightforwarder/status/transactionPast/PastTransactionFileList.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import PastTransactionTable from "./PastTransactionTable.tsx";
 import PastTransactionFilters from "./PastTransactionFilters.tsx"; // Import du composant de filtre
-import transactionData from "../../../../../../mocks/pasttransactions.json";
+import transactionData from "@mokes/pasttransactions.json";
 import { PastTransaction } from "../../../../../../types/PastTransaction.ts";
 import { useNavigate } from "react-router-dom";
 
